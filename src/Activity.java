ab8dac3651eac1ffd894531e21e1e94b9b087513
jogden330 <joogden@calpoly.edu>
2021-02-04 14:29:38 -0800

    Interface for action

---
+++ src/Activity.java
@@ -0,0 +1,58 @@
+public class Activity implements  Action{
+
+    private Entity entity;
+    private WorldModel world;
+    private ImageStore imageStore;
+
+
+    public Activity(
+
+            Entity entity,
+            WorldModel world,
+            ImageStore imageStore)
+
+    {
+
+        this.entity = entity;
+        this.world = world;
+        this.imageStore = imageStore;
+
+    }
+
+
+    public void executeAction(EventScheduler scheduler)
+    {
+        switch (entity.getKind()) {
+            case MINER_FULL:
+                entity.executeMinerFullActivity(world, imageStore, scheduler);
+                break;
+
+            case MINER_NOT_FULL:
+                entity.executeMinerNotFullActivity(world, imageStore, scheduler);
+                break;
+
+            case ORE:
+                entity.executeOreActivity(world, imageStore, scheduler);
+                break;
+
+            case ORE_BLOB:
+                entity.executeOreBlobActivity(world, imageStore, scheduler);
+                break;
+
+            case QUAKE:
+                entity.executeQuakeActivity(world,scheduler);
+                break;
+
+            case VEIN:
+                entity.executeVeinActivity(world, imageStore, scheduler);
+                break;
+
+            default:
+                throw new UnsupportedOperationException(String.format(
+                        "executeActivityAction not supported for %s",
+                        entity.getKind()));
+        }
+    }
+
+
+}
---
+++ src/Animation.java
@@ -0,0 +1,26 @@
+public class Animation implements Action{
+
+    private Entity entity;
+    private int repeatCount;
+
+    public Animation(
+
+            Entity entity,
+            int repeatCount)
+    {
+
+        this.entity = entity;
+        this.repeatCount = repeatCount;
+    }
+
+
+    public void executeAction(EventScheduler scheduler)
+    {
+        entity.nextImage();
+
+        if (repeatCount != 1) {
+            scheduler.scheduleEvent( entity, EntityFactory.createAnimationAction(entity,  Math.max(repeatCount - 1, 0)),  entity.getAnimationPeriod());
+        }
+    }
+
+}
